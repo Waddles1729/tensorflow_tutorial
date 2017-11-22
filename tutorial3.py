aa6a5964c92c4e65fb2a14721160d7b9ce9ac61f
Waddles1729 <masanari773@gmail.com>
2017-11-23 02:03:28 +0900

    new code

--- tutorial3.py
+++ tutorial3.py
@@ -5,7 +5,6 @@ import tensorflow as tf
 # Declare list of features. We only have one numeric feature. There are many
 # other types of columns that are more complicated and useful.
 feature_columns = [tf.feature_column.numeric_column("x", shape=[1])]
-sess = tf.Session()
 
 # An estimator is the front end to invoke training (fitting) and evaluation
 # (inference). There are many predefined types like linear regression,
