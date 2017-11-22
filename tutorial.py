4380c52d09f68bad38320358ef88606b7f412d83
Waddles1729 <masanari773@gmail.com>
2017-11-22 20:18:38 +0900

    new code

--- tutorial.py
+++ tutorial.py
@@ -11,3 +11,11 @@ print(sess.run([node1, node2]))
 node3 = tf.add(node1, node2)
 print("node3:", node3)
 print("sess.run(node3):", sess.run(node3))
+
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b
+
+print(sess.run(adder_node, {a: 3, b: 4.5}))
+print(sess.run(adder_node, {a: [1, 3], b: [2, 4]}))
+
